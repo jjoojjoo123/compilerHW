86170e9e7d923f0c9b2737338f0eb130e9da80f6
gimi65536 <gimi65536@hotmail.com>
2019-12-12 17:22:16 +0800

    Hw5 base

---
+++ hw5/gencode.c
@@ -0,0 +1,9 @@
+#include "header.h"
+#include "symboltable.h"
+#include "gencode.h"
+#include <stdio.h>
+#include <string.h>
+
+int label_number = 0;
+
+void gen_program(AST_NODE* programNode, FILE* output){}
---
+++ hw5/gencode.h
@@ -0,0 +1,9 @@
+#ifndef __GENCODE_H__
+#define __GENCODE_H__
+
+#include "header.h"
+#include <stdio.h>
+
+void gen_program(AST_NODE* programNode, FILE* output);
+
+#endif
